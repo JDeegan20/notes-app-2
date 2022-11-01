06221b1dbc7b82289c328605a91face1a99df074
JDeegan20 <20085768@mail.wit.ie>
2022-11-01 14:22:29 +0000

    Notes App - Refactoring listArchivedNotes function

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -42,19 +42,13 @@ class NoteAPI(serializerType: Serializer){
         }
     }
 
-    fun listArchivedNotes(): String {
-        return if (numberOfArchivedNotes() == 0) {
-            "No archived notes stored"
-        } else {
-            var listOfArchivedNotes = ""
-            for (note in notes) {
-                if (note.isNoteArchived) {
-                    listOfArchivedNotes += "${notes.indexOf(note)}: $note \n"
-                }
-            }
-            listOfArchivedNotes
-        }
-    }
+    fun listArchivedNotes(): String =
+        if (numberOfArchivedNotes() == 0) "No archived notes stored"
+        else notes.filter {note -> note.isNoteArchived}
+            .joinToString (separator = "\n" ) { note ->
+                notes.indexOf(note).toString() + ": " + note.toString() }
+
+
 
     fun listNotesBySelectedPriority(priority: Int): String {
         return if (notes.isEmpty()) {
